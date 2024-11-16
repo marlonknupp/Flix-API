97eabc42b6c173e651206f7de3f528586709777d
Marlon Knupp <marlonjcc23@gmail.com>
2024-11-16 20:18:37 -0300

    ...

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from genres.views import GenreCreateListView, genre_detail_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+
+    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
+]
