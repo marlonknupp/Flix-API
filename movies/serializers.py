0265cddd37f13ed4ee692153b29d6ff1825fcf66
Marlon Knupp <marlonjcc23@gmail.com>
2024-12-15 12:47:19 -0300

    ...

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,38 @@
 from rest_framework import serializers
 from movies.models import Movie
+from django.db.models import Avg
 
-class MovieSerializer(serializers.ModelSerializer):
+
+
+class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
-        fields = '__all__'
+        field = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        
+        return None
+    
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError(' A data de lançamento nao pode ser anterior a 1900')
+        return value
+    
+    def validate_resume(self, value):
+        if len (value) >500:
+            raise serializers.ValidationError('Resumo nao deve ser maior do que 500 caracteres.')
+        return value
+
+
+class MovieStatsSerializer(serializers.Serializer):
+    total_movies = serializers.IntegerField()
+    movies_by_genre = serializers.ListField()
+    total_reviews = serializers.IntegerField()
+    avarage_stars = serializers.FloatField()
+    
--- movies/views.py
+++ movies/views.py
@@ -1,16 +1,39 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
+from django.db.models import Count, Avg
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
-from movies.serializers import MovieSerializer
+from movies.serializers import MovieModelSerializer, MovieStatsSerializer
 from app.permissions import GlobalDefaultPermission
+from reviews.models import Review
 
 class MovieCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+    serializer_class = MovieModelSerializer
 
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+    serializer_class = MovieModelSerializer
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+
+    def get (self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre_name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
+        data={
+            'total_movies': total_movies,
+            'movies_by_genre': movies_by_genre,
+            'total_review': total_reviews,
+            'average_stars': round(average_stars, 1) if average_stars else 0,
+        }
+        serializer = MovieStatsSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        return response.Response(data=dict(), status=status.HTTP_200_OK,)
